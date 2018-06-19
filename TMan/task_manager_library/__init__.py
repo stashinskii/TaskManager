be031315c473b98d88c30ab40225886900d4d038
Herman Stashynki <germanstashinskii@gmail.com>
2018-06-19 17:16:50 +0300

    Fixes

--- TMan/task_manager_library/__init__.py
+++ TMan/task_manager_library/__init__.py
@@ -1,4 +1,50 @@
-from console.actions import *
-from task_manager_library.models.user_model import *
-from console.actions import *
+r"""
+This library designed to use in task manager
+
+How to use library in your app:
+
+Firstly, import this library:
+    >>> import task_manager_library
+
+There are 2 main task controllers, designed to manage your entities, receive request from
+app, return objects and exceptions
+
+Main entities represents in models:
+
+1. Task
+2. Priority
+3. Status
+4. Tag
+5. Scheduler
+
+It use JSON serializator for manage data in Storage and SchedulerStorage classes. SchedulerStorage
+designed for managing scheduler data at JSON files.
+
+Storage:
+
+Firstly, class initialize new json files if needed
+
+    >>> from task_manager_library.storage import task_storage
+
+To load data and deserialize it use:
+    >>> task_storage.load_tasks_from_json()
+
+It stores data in Storage - tasks
+To get users tasks use:
+    >>> task_storage.load_user_tasks()
+
+It stores data in Storage - user_tasks
+To get more information - get help on task_manager_library.storage.task_storage
+
+Same actions used for SchedulerStorage
+
+TaskController:
+Designed for managing tasks
+
+To create instance, send Storage's object
+
+For more help use help on task_manager_library.controollers.task_controller
+
+
+"""
 
