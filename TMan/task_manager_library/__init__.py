bb2455c289ab65b8754628f38af80c218c1af85d
Herman Stashynki <germanstashinskii@gmail.com>
2018-06-22 22:19:12 +0300

    Fixes

--- TMan/task_manager_library/__init__.py
+++ TMan/task_manager_library/__init__.py
@@ -17,33 +17,57 @@ Main entities represents in models:
 4. Tag
 5. Scheduler
 
-It use JSON serializator for manage data in Storage and SchedulerStorage classes. SchedulerStorage
-designed for managing scheduler data at JSON files.
+It use JSON serializator for manage data in Storage and SchedulerStorage
+classes. SchedulerStorage designed for managing scheduler data at JSON files.
+----------------------------------------------------------------------------------------------
 
-Storage:
+-- USAGE --
 
-Firstly, class initialize new json files if needed
+-- STORAGE USAGE --
 
+0. Firstly, class initialize new json files if needed
     >>> from task_manager_library.storage import task_storage
 
-To load data and deserialize it use:
-    >>> task_storage.load_tasks_from_json()
+1. Create new storage instance
+    >>> task = Storage()
+    Or send new path to storage:
+    >>> task = Storage("new/path")
+2. Use storage methods like this:
+    >>> task.load_tasks_from_json()
 
-It stores data in Storage - tasks
-To get users tasks use:
-    >>> task_storage.load_user_tasks()
+After loading tasks: task.load_tasks_from_json() you may get
+access to your tasks by using task.tasks
 
-It stores data in Storage - user_tasks
 To get more information - get help on task_manager_library.storage.task_storage
 
-Same actions used for SchedulerStorage
+----------------------------------------------------------------------------------------------
 
-TaskController:
-Designed for managing tasks
+-- TASK CONTROLLER USAGE --
+Main thing which is manage your library is TaskController
 
-To create instance, send Storage's object
+How to work with your controller:
+0. Import your TaskController module from library:
+    >>> from task_manager_library.controllers.task_controller import TaskController
+1. Create TaskController instance
+    >>> task_controller = TaskController(storage)
+    storage - is instance of Storage class (information how to create it written in Storage part)
+2. Use methods to manage you task manager!
 
-For more help use help on task_manager_library.controollers.task_controller
+Examples of usage TaskController:
+1. Add new task:
+    >>> task = Task(title="New title", Tag("tag-name"))
+    >>> task_controller.add(task)
+
+2. Edit task
+    >>> task_controller.edit("55545s6d-45ds456ds456d-s45d6s46dsds", title="New title")
+
+3. Delete task
+    >>> task_controller.delete("55545s6d-45ds456ds456d-s45d6s46dsds")
+
+Import module and use help to get more methods
+
+
+
 
 
 """
